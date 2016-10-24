af34e682579c5b085af8ec99d767ed93f82249b0
Allan MacGregor <amacgregor@allanmacgregor.com>
2016-10-23 20:03:12 -0400

    Write Select Sort first attempt

--- lib/sort.ex
+++ lib/sort.ex
@@ -1,2 +1,15 @@
 defmodule Sort do
+  def ssort([]), do: []
+  def ssort(list), do: selection_sort(list)
+
+  defp selection_sort([]), do: []
+  defp selection_sort(list) do
+    min = get_min(list)
+    [min | selection_sort(List.delete(list, min))]
+  end
+
+  defp get_min([h|[]]), do: h 
+  defp get_min([f|[s|tail]]) when f < s, do: get_min([f|tail])
+  defp get_min([f|[s|tail]]) when f > s, do: get_min([s|tail])
+
 end
--- test/sort_test.exs
+++ test/sort_test.exs
@@ -2,7 +2,11 @@ defmodule SortTest do
   use ExUnit.Case
   doctest Sort
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "handle empty list" do
+    assert Sort.ssort([]) == []
+  end
+  test "basic selection sort test" do 
+    assert Sort.ssort([100,200,10,1]) == [1,10,100,200]
+    assert Sort.ssort([9,8,7,6,5,4,3,2,1]) == [1,2,3,4,5,6,7,8,9]
   end
 end
